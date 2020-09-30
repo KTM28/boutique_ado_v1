f7967799433d969f1ebfe6f1704533822eef6175
KTM28 <vabishya.gautam@gmail.com>
2020-09-30 15:45:58 +0100

    started product template

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -162,5 +162,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIA_URL = '/media/pics/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/pics')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
